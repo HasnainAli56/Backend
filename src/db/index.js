39e0e4c6b536bef52ca08283e6f6ec0565d73604
Hasnain Ali Bashir <hasnainali945537@gmail.com>
2025-08-05 11:13:39 +0500

    coonection to db

---
+++ src/db/index.js
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose';
+import { DB_NAME } from '../constants.js';
+
+const connectDB = async() => {
+    try {
+        console.log('MONGODB_URI:', process.env.MONGODB_URI); // Debug line
+        console.log('DB_NAME:', DB_NAME); // Debug line
+        const connectionInstance = await mongoose.connect(
+            `${process.env.MONGODB_URI}/${DB_NAME}`
+        );
+        console.log(
+            `\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`
+        );
+    } catch (error) {
+        console.error('MONGODB connection error ', error);
+        process.exit(1);
+    }
+};
+
+export default connectDB;
