5aa08a8de517b72aa2f4d4ee9ee0725aa49c3acc
Hasnain Ali Bashir <hasnainali945537@gmail.com>
2025-08-06 09:11:00 +0500

    utils code

--- src/index.js
+++ src/index.js
@@ -8,24 +8,31 @@ dotenv.config({
 })
 
 connectDB()
-
-/*
-import express from "express"
-const app = express()
-    (async() => {
-        try {
-            await mongoose.connect(`${process.env.MONGODB_URI}/ ${DB_NAME}`)
-            app.on("error", (error) => {
+    .then(() => {
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`server is running at port ${process.env.PORT}`)
+        })
+    })
+    .catch((err) => {
+        console.log("Mongodb connection error!!", err)
+    })
+    /*
+    import express from "express"
+    const app = express()
+        (async() => {
+            try {
+                await mongoose.connect(`${process.env.MONGODB_URI}/ ${DB_NAME}`)
+                app.on("error", (error) => {
+                    console.log("error", error)
+                    throw error
+                })
+                app.listen(process.env.PORT, () => {
+                    console.log(`App is listening on port ${process.env.PORT}`)
+                })
+            } catch (error) {
                 console.log("error", error)
                 throw error
-            })
-            app.listen(process.env.PORT, () => {
-                console.log(`App is listening on port ${process.env.PORT}`)
-            })
-        } catch (error) {
-            console.log("error", error)
-            throw error
 
-        }
-    })())
-        */
+            }
+        })())
+            */
