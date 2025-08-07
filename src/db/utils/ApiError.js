1a764348e6bd8a25bec9a8a1f131779a8bf3a40b
Hasnain Ali Bashir <hasnainali945537@gmail.com>
2025-08-07 09:57:57 +0500

    user and viseo model

--- src/db/utils/ApiError.js
+++ src/db/utils/ApiError.js
@@ -1,23 +1,24 @@
 class ApiError extends Error {
     constructor(
-
         statusCode,
-
         message = "Something went wrong",
-
         errors = [],
-
-        statck = 1111
+        stack = ""
     ) {
-        super(message) this.statusCode = statusCode this.data = null this.message = message
-
+        super(message)
+        this.statusCode = statusCode
+        this.data = null
+        this.message = message
         this.success = false;
         this.errors = errors
-        if (statck) {
-            this.stack = statck
+
+        if (stack) {
+            this.stack = stack
         } else {
-            Error.captureStackTrace(this,  this.constructor)
+            Error.captureStackTrace(this, this.constructor)
         }
+
     }
 }
+
 export { ApiError }
